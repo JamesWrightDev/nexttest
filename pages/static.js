5ecd260d8e122c1ccaa7e0c68ad734b4c9e16260
James <contact@james-wright.co.uk>
2020-05-18 02:47:58 +0100

    adds static

--- pages/static.js
+++ pages/static.js
@@ -1,13 +1,27 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function staticpage() {
+const Static = ({ data }) => {
   return (
     <div className="container">
-      <Link href="/search">
-        Search
+      <Link href="/">
+        <a>Search</a>
       </Link>
-      <h1>Static</h1>
+      {
+        data.map(item => {
+          return(
+            <li key={item.id}>{item.title}</li>
+          )
+        })
+      }
     </div>
-  )
-}
+  );
+};
+
+export async function getStaticProps(context) {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts`
+  );
+  const json = await res.json();
+  return { props: {data: json} };
+};
+export default Static;
